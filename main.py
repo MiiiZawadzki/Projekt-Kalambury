140c62fdc1c62a5331c35d5735e9d948ad9c9361
Michal <zawadzki.micha@gmail.com>
2021-04-08 23:18:04 +0200

    createRoom improvement
    Add: turn length and count in db and models.py; labels to inputs in createRoom and js to handle displaying new values

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ def create_room():
         turn_count = request.form["turn_count"]
 
         words = return_words_string()
-        room = Room(room_id=session['room_id'], admin_username=session["username"], current_word="", words=words, who_draws=session["username"])
+        words_array = words.split(';')
+        room = Room(room_id=session['room_id'], admin_username=session["username"], current_word=words_array[0], words=words, who_draws=session["username"], turn_count=turn_count, turn_length=turn_length)
         db.session.add(room)
         db.session.commit()
 
