3451462b8025255f7f7f7c3a7bd087bd9c93a624
Michal <zawadzki.micha@gmail.com>
2021-05-28 17:44:05 +0200

    Change brush thickness

--- static/js/game.js
+++ static/js/game.js
@@ -1,5 +1,5 @@
 var color = "black",
-  thickness = 16;
+  thickness = 8;
 var timer = null;
 var timeEnd = false;
 let user = sessionStorage.getItem("username");
@@ -92,13 +92,13 @@ $(function () {
     var t = $(this).attr("id");
     switch (t) {
       case "max-width":
-        thickness = 22;
-        break;
-      case "medium-width":
         thickness = 16;
         break;
+      case "medium-width":
+        thickness = 8;
+        break;
       case "small-width":
-        thickness = 12;
+        thickness = 2;
         break;
     }
   });
